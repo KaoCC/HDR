11d1941337fd005feb2d814646d7fcd61fb5de9c
Chih-Chen Kao <elros.morlin@gmail.com>
2017-04-08 21:03:54 +0800

    add solver structure
    add solver structure

---
+++ HDRI/include/LinearLeastSquares.hpp
@@ -0,0 +1,33 @@
+#ifndef _HDRI_LINEARLEASTSQARES_HPP_
+#define _HDRI_LINEARLEASTSQARES_HPP_
+
+
+
+
+namespace HDRI {
+
+
+	class LinearLeastSquares {
+
+	public:
+
+		LinearLeastSquares() = delete;
+
+		static void solver();
+
+
+	};
+
+}
+
+
+
+
+
+
+
+
+
+
+#endif
+
